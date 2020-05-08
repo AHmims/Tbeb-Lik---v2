3522c262039284c6f57b8f5e4bf6371604757bed
AHmims <ali.hmims99@gmail.com>
2020-05-08 21:04:52 +0000

    minor db connect err handling

--- model/dbConfig.js
+++ model/dbConfig.js
@@ -1,15 +1,20 @@
 const mysql = require('mysql2/promise');
 require('dotenv').config();
 // 
-var pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
-});
+var pool = null;
+try {
+    pool = mysql.createPool({
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
+        waitForConnections: true,
+        connectionLimit: 10,
+        queueLimit: 0
+    });
+} catch (err) {
+    console.error('You dont have access to this DB !');
+}
 const connect = () => {
     return pool.getConnection();
 }
