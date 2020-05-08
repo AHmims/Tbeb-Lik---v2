56daef0ed0a178d2739841f3788ea9a879f91dd2
AHmims <ali.hmims99@gmail.com>
2020-05-08 05:23:47 +0000

    removed a miss type

--- public/js/medecin/socketChat.js
+++ public/js/medecin/socketChat.js
@@ -4,7 +4,7 @@ let __PEER;
 // 
 __GLOBAL_SOCKET.on('connect', () => {
     console.log('Socket Connected ! userId => ', sessionStorage.getItem('matricule') || null);
-    await __GLOBAL_SOCKET.emit('newUser', sessionStorage.getItem('matricule'));
+    __GLOBAL_SOCKET.emit('newUser', sessionStorage.getItem('matricule'));
     joinRoom();
 });
 // 
