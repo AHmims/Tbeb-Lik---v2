c54d85167ce91c2e24a581af707e1c5cc26ae2f1
AHmims <ali.hmims99@gmail.com>
2020-05-09 15:11:32 +0000

    another fix

--- public/js/medecin/viewChat.js
+++ public/js/medecin/viewChat.js
@@ -1,7 +1,9 @@
 function createMsgBox(msg, type) {
-    var txt = document.createElement('h4');
-    txt.setAttribute('class', `msgTxt ${type}`);
-    txt.innerText = msg.CONTENU;
+    var container = document.createElement('div');
+    container.setAttribute('class', `messageContainer ${type}`);
+    var txt = document.createElement('span');
+    txt.innerText = msg.contenu;
     // 
-    document.getElementById('msgsCont').appendChild(txt);
+    container.appendChild(txt);
+    document.getElementById('msgsCont').appendChild(container);
 }
