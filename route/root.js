3a8c4d5801f4453044e54413b3a387d88a8a112f
AHmims <ali.hmims99@gmail.com>
2020-08-24 18:27:16 +0100

    history for clients

--- route/root.js
+++ route/root.js
@@ -24,12 +24,13 @@ router.get('/dashboard', isAuth, async (req, res) => {
         _data.refCode = await getRefCode(req.user.userId);
         _data.inbox = await getClientNotifications(req.user.userId); // PRECONSULTATIONS
         _data.clients = await getConsultations(req.user.userId); //CONSULTATIONS
+        // _data.history = await 
     } else {
         _data.ongoing = await canSendPrecons(req.user.userId); //LAST PRECONSULTATION
         if (_data.ongoing == false) {
-            _data.consul = await visitorCurrentConsultation(req.user.userId);
+            _data.consul = await visitorCurrentConsultation(req.user.userId); //CONSULTATIO EN COURS
         }
-        _data.history = await getVisitorConsultations(req.user.userId);
+        _data.history = await getVisitorConsultations(req.user.userId); // HISTORY OF PAST CONSULTATIONS
     }
     // console.log(_data);
 
