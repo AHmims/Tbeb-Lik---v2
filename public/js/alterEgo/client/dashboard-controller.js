8f98a4bc0a9de93c2443eaa53e54aa47f0548c83
AHmims <ali.hmims99@gmail.com>
2020-08-28 19:34:24 +0100

    client refcode section

--- public/js/alterEgo/client/dashboard-controller.js
+++ public/js/alterEgo/client/dashboard-controller.js
@@ -1,4 +1,7 @@
 $().ready(() => {
+    // 
+    share_ref();
+    // 
     $('.notif_box').each(function (i, element) {
         const _BOX_ID = $(this).attr('id');
         const _VISITOR_ID = document.getElementsByClassName('notif_box_user')[i].getAttribute('id');
@@ -25,4 +28,35 @@ function acceptNotification(notifId, visitorId) {
 function remove_precons(notifId) {
     if (document.getElementById(notifId))
         document.getElementById(notifId).remove();
+}
+// 
+const share_ref = () => {
+    const share_btn = document.getElementById('refBtn_share');
+    if (share_btn) {
+        share_btn.addEventListener('click', () => {
+            // if (navigator.share) {
+            // Implement Later
+            // } else {
+            // }
+            // 
+            const ref_code = document.getElementById('refCode');
+            // 
+            ref_code.focus();
+            ref_code.select();
+            let successful = document.execCommand('copy');
+            if (success) {
+                ref_code.blur();
+                success();
+            }
+
+        });
+    }
+    // 
+    function success() {
+        const old_content = share_btn.innerHTML;
+        share_btn.innerHTML = "&#10004;";
+        setTimeout(() => {
+            share_btn.innerHTML = old_content;
+        }, 3000);
+    }
 }
